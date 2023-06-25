8364b8782a83944f4b6e071841dc905be764e777
VuHuy <huyv80313@gmail.com>
2023-06-25 22:34:33 +0700

    add class Tree

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -6,19 +6,32 @@ class Node:
         self.left = left
         self.right = right
     def hasLeftChile(self, p):
-        pass
+        if p.left == None: return False
+        return True
     
     def hasRightChile(self, p):
-        pass
+        if p.right == None: return False
+        return True
     
     def addLeftChild(self, p, item):
-        pass
-    
+        if self.hasLeftChile(p):
+            print("Không thêm được nữa")
+            return 1
+        newNode = Node(item)
+        p.left = newNode
+        # size +=1 
+            
     def addRightChild(self, p, item):
-        pass
+        if self.hasRightChile(p):
+            print("Không thêm được nữa")
+            return 1
+        newNode = Node(item)
+        p.right = newNode
+        # size +=1
 
 class Tree:
     def __init__(self,root, size):
         self.root = root
         self.size = size
-    
+        
+        
